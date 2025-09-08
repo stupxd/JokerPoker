830da7301f558ad0081eb8b8e36ce40f0dcbe3d8
stupxd <uknownvector@gmail.com>
2025-09-08 21:59:38 +0300

    Initial commit

---
+++ localization/en-us.lua
@@ -0,0 +1,12 @@
+return {
+    misc = {
+        challenge_names = {
+            c_jopo_joker_poker = "Joker Poker",
+        },
+        v_text = {
+            ch_c_jopo_joker_poker={
+                "Start every round with {C:attention}random{} set of Jokers",
+            },
+        },
+    }
+}
---
+++ main.lua
@@ -0,0 +1,82 @@
+SMODS.Challenge {
+    key = "joker_poker",
+
+    rules = {
+        custom = {
+            {id = 'jopo_joker_poker', value = 69},
+        },
+        modifiers = {}
+    },
+}
+
+
+--- Remove all current jokers, fill up joker slots with random jokers
+--- @param run_start any
+local function joker_poker_jokers(run_start)
+
+    -- Must be blocking so that new jokers only generate after old ones are dissolved (and no longer "block")
+    local blocking = not run_start
+    local _first_dissolve = false
+
+    local negatives = 0
+    G.E_MANAGER:add_event(Event {blocking = blocking, trigger = 'before', delay = 0.75, func = function()
+        for k, v in ipairs(G.jokers.cards) do
+            if v.edition and v.edition.negative then
+                negatives = negatives + 1
+            end
+            v:start_dissolve(nil, _first_dissolve)
+            _first_dissolve = true
+        end
+        return true end } )
+
+    local all_legendary = pseudorandom("joker_poker_all_legendary") < 0.01
+
+    G.E_MANAGER:add_event(Event {blocking = blocking, trigger = 'before', delay = 0.4, func = function()
+        local card_limit = G.jokers.config.card_limit or G.jokers.config.card_limits.card_limit
+
+        for i = 1,  - negatives do
+            local legendary = all_legendary or pseudorandom("joker_poker_legendary") < 0.01
+            local card = SMODS.create_card {
+                key_append = "joker_poker",
+                set = "Joker", 
+                stickers = {"eternal"},
+                force_stickers = true,
+                rarity = legendary and "Legendary" or nil
+            }
+            card:start_materialize()
+            card:add_to_deck()
+            G.jokers:emplace(card)
+        end
+        return true end } )
+end
+
+
+--#region Hooks
+
+local game_start = Game.start_run
+
+function Game:start_run(args)
+    game_start(self, args)
+
+    if args.savetext then
+        return
+    end
+
+    if not args.challenge or args.challenge.id ~= "c_jopo_joker_poker" then
+        debugplus.log(args.challenge)
+        return
+    end
+
+    joker_poker_jokers(true)
+end
+
+
+local round_end = G.FUNCS.cash_out
+
+G.FUNCS.cash_out = function(e)
+    round_end(e)
+
+    joker_poker_jokers(false)
+end
+
+--#endregion Hooks
